9f34963c606e4cc1bfe14b53aab463cb0945e084
Mike <michalis.e@onenovation.com>
2024-08-06 16:59:14 +0300

    neo-tree testing

--- lua/mike/lazy/telescope.lua
+++ lua/mike/lazy/telescope.lua
@@ -12,7 +12,8 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>g', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+        -- vim.keymap.set('n', '<leader>g', builtin.git_files, {})
         vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
     end
 }
--- lua/mike/remaps.lua
+++ lua/mike/remaps.lua
@@ -4,3 +4,5 @@ vim.keymap.set("n", "<leader>\\", vim.cmd.Ex)
 
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>")
