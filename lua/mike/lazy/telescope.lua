c24ff81bdd9a5e3618ae84bf2a19db4ef281c5e1
Mike <michalis.e@onenovation.com>
2024-08-23 07:12:02 +0300

    lsp-none, etc.

--- lua/mike/lazy/telescope.lua
+++ lua/mike/lazy/telescope.lua
@@ -1,20 +1,36 @@
 return {
-    "nvim-telescope/telescope.nvim",
+    {
+        "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+        tag = "0.1.5",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim"
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        },
+
+        config = function()
+            require('telescope').setup({})
+
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+            -- vim.keymap.set('n', '<leader>g', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
+        end
     },
-
-    config = function()
-        require('telescope').setup({})
-
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
-        -- vim.keymap.set('n', '<leader>g', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
-    end
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+                        }
+                    }
+                }
+            }
+            require("telescope").load_extension("ui-select")
+        end
+    }
 }
 
--- lua/mike/lazy/treesitter.lua
+++ lua/mike/lazy/treesitter.lua
@@ -4,17 +4,12 @@ return {
     config = function()
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
-            ensure_installed = {
-                "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash", "powershell", "json", "yaml", "html", "css", "json",
-            },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
+           -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
 
             -- Automatically install missing parsers when entering buffer
             -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            auto_install = false,
+            auto_install = true,
 
             indent = {
                 enable = true
