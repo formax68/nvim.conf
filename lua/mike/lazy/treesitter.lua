a83e3b9cce049b8f6bbf58dc53ef3e6d5e48ede0
Mike <michalis.e@onenovation.com>
2024-09-03 16:42:06 +0300

    this is where we at

--- lua/mike/lazy/treesitter.lua
+++ lua/mike/lazy/treesitter.lua
@@ -34,7 +34,7 @@ return {
                 files = {"src/parser.c", "src/scanner.c"},
                 branch = "master",
             },
-        }
+       }
 
         vim.treesitter.language.register("templ", "templ")
     end
